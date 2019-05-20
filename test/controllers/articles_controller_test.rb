d20d2fdf34d6d94a545d773806b8baab2d1b56da
Joshua Grosser <joshua.grosser@gmail.com>
2019-05-20 10:38:51 -0400

    Added comments model, schema, etc.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "test001" do
+    
+  end
 end
